7ede9f0e7586b53fb84b7960844bfab9083e861b
amankeldi.kurban <dimitesting31@gmail.com>
2024-12-14 13:31:51 +0200

    Deserialized json data received from public API

--- Program.cs
+++ Program.cs
@@ -1,12 +1,64 @@
-﻿
+﻿using System.Net.Http;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace ApiReader
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             Console.WriteLine("New project to Read from public API and print stats per user request");
+            string baseAddress = "https://swapi.dev/api/";
+            string requestUri = "planets";
+
+            IApiDataReader apiDataReader = new Api();
+            string jsonResponse = await apiDataReader.Read(baseAddress, requestUri);
+            Planets planets = JsonSerializer.Deserialize<Planets>(jsonResponse);
+            CustomPrinter customPrinter = new CustomPrinter();
+            //customPrinter.Print(planets);
+            customPrinter.PrintProperies();
+        }
+
+        public interface IApiDataReader
+        {
+            Task<string> Read(string baseAddress, string requestUri);
+        }
+
+        public class Api : IApiDataReader
+        {
+            public async Task<string> Read(string baseAddress, string requestUri)
+            {
+                using var client = new HttpClient();
+                client.BaseAddress = new Uri(baseAddress);
+                HttpResponseMessage response = await client.GetAsync(requestUri);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync(); 
+            }
+        }
+        public record PlanetProperties(
+            [property: JsonPropertyName("name")] string name,
+            [property: JsonPropertyName("diameter")] string diameter,
+            [property: JsonPropertyName("surface_water")] string surface_water,
+            [property: JsonPropertyName("population")] string population
+        );
+
+        public record Planets(
+            [property: JsonPropertyName("results")] IReadOnlyList<PlanetProperties> results
+        );
+
+        public class CustomPrinter
+        {
+            private readonly Planets _planets; 
+
+            public void Print(Planets planets)
+            {
+                foreach(var planet in planets.results)
+                {
+                    Console.WriteLine($"{planet.name}");
+                }
+            }
         }
     }
 }
--- obj/Debug/net8.0/ApiReader.AssemblyInfo.cs
+++ obj/Debug/net8.0/ApiReader.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiReader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e2a356904a0e733d3d1db3e4d94ab0ae43ab2a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiReader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiReader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
