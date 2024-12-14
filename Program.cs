7e2a356904a0e733d3d1db3e4d94ab0ae43ab2a6
amankeldi.kurban <dimitesting31@gmail.com>
2024-12-14 12:39:16 +0200

    initial setup

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace ApiReader
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("New project to Read from public API and print stats per user request");
+        }
+    }
+}
